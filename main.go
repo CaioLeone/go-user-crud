2129d9ff4a5bc5215ddb48fef99cae6200a05675
Caio <caio_leone@hotmail.com>
2026-03-15 21:08:06 -0300

    fix: main

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main (){
+import (
+	"fmt"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	fmt.Println("Crud usando CHI")
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
 	
 }
