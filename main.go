31fed28c8d1499f142f5dc061a55de184e0f0b97
Caio <caio_leone@hotmail.com>
2026-03-22 21:53:40 -0300

    fix: main go

--- main.go
+++ main.go
@@ -1,13 +1,35 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
 	"net/http"
+	"time"
 
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	"github.com/caioleone/go-user-crud/api"
 )
 
 func main() {
-	fmt.Println("Crud usando CHI")
+	if err := run(); err != nil {
+		slog.Error("Failed to execute code", "error", err)
+		return
+	}
+	slog.Info("All Systems Offline")
+}
+
+func run() error {
+	db := make(map[string]string)
+
+	handler := api.NewHandler(db)
+	s := http.Server{
+		ReadTimeout:  10 * time.Second,
+		IdleTimeout:  time.Minute,
+		WriteTimeout: 10 * time.Second,
+		Addr:         ":8080",
+		Handler:      handler,
+	}
+
+	if err := s.ListenAndServe(); err != nil {
+		return err
+	}
+	return nil
 }
