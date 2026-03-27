6e29ad2f333056d2c6db0c13de8f4db36180bc3d
Caio <caio_leone@hotmail.com>
2026-03-27 18:57:40 -0300

    fix: api funcs

--- api/api.go
+++ api/api.go
@@ -48,7 +48,97 @@ func NewHandler(db map[string]string) http.Handler {
 	return route
 }
 
+// CREATE USER
+func handleCreateUser(db map[string]entity.User) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var user entity.User
 
+		if err := json.Decoder(r.Body).Decode(&user); err != nil {
+			sendJson(w, Response{Error: "Invalid Body"}, http.StatusBadRequest)
+			return
+		}
+
+		//VALIDACAO
+		if len(user.FirstName) < 2 || len(user.LastName) < 2 || len(user.Biography) < 20 {
+			sendJson(w, Response{Error: "Invalid fields"}, http.StatusBadRequest)
+			return
+		}
+
+		user.ID = uuid.New().String()
+		db[user.ID] = user
+
+		sendJson(w, Response{Data: user}, http.StatusCreated)
+	}
+}
+
+// GET USER
+func handleGetAllUsers(db map[string]entity.User) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var users []entity.User
+
+		for _, u := range db{
+			users, append(users, u)
+		}
+
+		sendJson(w, Response{Data: users}, http.StatusOK)
+	}
+}
+
+// GET BY ID
+func handleGetById(db map[string]entity.User) http.HandlerFunc{
+	return func(w http.ResponseWriter, r *http.Request) {
+		id := chi.URLParam(r, "id")
+
+		user, ok := db[id]
+		if !ok {
+			sendJson(w, Response{Error: "User not found"}, htpp.StatusNotFound)
+			return
+		}
+
+		sendJson(w, Response{Data: user}, http.StatusOK)
+	}
+}
+
+//UPDATE
+func handleUpdateUser(db map[string]entity.User) http.HandlerFunc{
+	return func(w http.ResponseWriter, r *http.Request) {
+		id := chi.URLParam(r, "id")
+
+		_, ok := db[id]
+		if !ok {
+			sendJson(w, Response{Error: "User not found"}, htpp.StatusNotFound)
+			return
+		}
+
+		var updated entity.User
+		if err := json.NewDecoder(r.Body).Decode(&updated); err != nil {
+			sendJson(w, Response{Error: "Invalid Body"}, http.StatusBadRequest)
+			return 
+		}
+
+		updated.ID = id
+		db[id] = updated
+
+		sendJson(w, Response{Data: updated}, http.StatusOK)
+	}
+}
+
+//DELETE
+func handleDeleteUser(db map[string]entity.User) http.HandlerFunc{
+	return func(w http.ResponseWriter, r *http.Request) {
+		id := chi.URLParam(r, "id")
+
+		user, ok := db[id]
+		if !ok{
+			sendJson(w, Response{Error: "User not found"}, http.StatusNotFound)
+			return
+		}
+
+		delete(db, id)
+
+		sendJson(w, Response{Data: user}, http.StatusOK)
+	}
+}
 
 func sendJson(w http.ResponseWriter, resp Response, status int) {
 	w.Header().Set("Content-Type", "application/json")
